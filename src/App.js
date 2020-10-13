040f36d1f8dd98bb08e4f26c571deb9905941905
Nathan-Vanstaevel <nathanvanstaevel@gmail.com>
2020-10-13 17:01:19 +0200

    React exercise 2

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 import Contact from './components/Contact';
 
 function App() {
-  return <Contact {...person} />;
+  return (
+    <div className='App'>
+      <Contact {...person1} />
+      <Contact {...person2} />
+      <Contact {...person3} />
+    </div>
+  );
 }
 
-const person = {
+const person1 = {
   Name: 'Kaylee Jones',
   Avatar: 'https://randomuser.me/api/portraits/women/6.jpg',
+  Online: true,
+};
+
+const person2 = {
+  Name: 'Earl Arnold',
+  Avatar: 'https://randomuser.me/api/portraits/men/14.jpg',
+  Online: false,
+};
+
+const person3 = {
+  Name: 'Leah Dean',
+  Avatar: 'https://randomuser.me/api/portraits/women/95.jpg',
   Online: true,
 };
 
