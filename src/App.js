0ca03825d7aa8b74b653e7a7869893d942b16e25
Nathan-Vanstaevel <nathanvanstaevel@gmail.com>
2020-10-14 14:03:11 +0200

    React state exercise

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
 import React from 'react';
-import Contact from './components/Contact';
+import ContactList from './components/ContactList';
 
 function App() {
   return (
     <div className='App'>
-      <Contact {...person1} />
-      <Contact {...person2} />
-      <Contact {...person3} />
+      <ContactList />
     </div>
   );
 }
-
-const person1 = {
-  Name: 'Kaylee Jones',
-  Avatar: 'https://randomuser.me/api/portraits/women/6.jpg',
-  Online: true,
-};
-
-const person2 = {
-  Name: 'Earl Arnold',
-  Avatar: 'https://randomuser.me/api/portraits/men/14.jpg',
-  Online: false,
-};
-
-const person3 = {
-  Name: 'Leah Dean',
-  Avatar: 'https://randomuser.me/api/portraits/women/95.jpg',
-  Online: true,
-};
 
 export default App;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import './Chat.css';
 
-function Contact({ Name, Avatar, Online }) {
-  return (
-    <div className='Contact'>
-      <img className='avatar' src={Avatar} alt={Name} />
-      <div className='name'>
-        {Name}
-        <p className='status'>
-          <span className='status-online'></span>
-          {Online ? 'online' : 'offline'}
-        </p>
+class Contact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      online: true,
+    };
+  }
+
+  render() {
+    return (
+      <div className='Contact'>
+        <img className='avatar' src={this.props.avatar} alt={this.props.name} />
+        <div className='name'>
+          {this.props.name}
+          <p className='status'>
+            <span
+              className={this.state.online ? 'status-online' : 'status-offline'}
+              onClick={(event) => {
+                const newOnline = !this.state.online;
+                this.setState({ online: newOnline });
+              }}
+            ></span>
+            {this.state.online ? 'online' : 'offline'}
+          </p>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
+
+// function Contact(props) {
+//   return (
+//     <div className='Contact'>
+//       <img className='avatar' src={props.avatar} alt={props.name} />
+//       <div className='name'>
+//         {props.name}
+//         <p className='status'>
+//           <span className='status-online'></span>
+//           {props.online ? 'online' : 'offline'}
+//         </p>
+//       </div>
+//     </div>
+//   );
+// }
 
 export default Contact;
